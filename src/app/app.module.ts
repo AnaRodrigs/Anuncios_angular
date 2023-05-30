de47eb557b7e92879ad6781bcd83c0e0b6b21221
AnaRodrigs <mariaanarodrigues2005@gmail.com>
2023-05-30 16:24:57 -0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GaragemComponent } from './garagem/garagem.component';
 import { VendedorComponent } from './vendedor/vendedor.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { VendedorComponent } from './vendedor/vendedor.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
