8d2594dad77995d06a990e69053790817ff48b68
AnaRodrigs <mariaanarodrigues2005@gmail.com>
2023-05-30 17:14:47 -0300

    estrutura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GaragemComponent } from './garagem/garagem.component';
+import { VendedorComponent } from './vendedor/vendedor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'garagem', component: GaragemComponent},
+{path: 'vendedor', component: VendedorComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { GaragemComponent } from './garagem/garagem.component';
 import { VendedorComponent } from './vendedor/vendedor.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GaragemComponent,
-    VendedorComponent
+    VendedorComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
